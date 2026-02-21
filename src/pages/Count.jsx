32dc1c9f55e8e4aa739f6acb0ad18985c03e9dd0
Ting Tong <103483140+athipaty@users.noreply.github.com>
2026-02-21 12:14:19 +0800

    Update Count.jsx

--- src/pages/Count.jsx
+++ src/pages/Count.jsx
@@ -97,7 +97,7 @@ export default function Count() {
 
   return (
     <div className="min-h-screen bg-gray-100 p-4">
-              <div className="max-w-md mx-auto bg-white rounded-xl shadow-sm border border-gray-100 p-4 space-y-6
+              <div className="max-w-md mx-auto bg-white rounded-xl shadow-sm border border-gray-100 p-4 space-y-2
                 animate-fade-in">
           {/* Header */}
           <div>
